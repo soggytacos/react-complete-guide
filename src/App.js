66726ed40aedccea60e2128ac13b0ac95a9f67dd
Colman <colman@Transactlys-MacBook-Pro.local>
2020-02-14 15:13:34 -0600

    I'm about to delete the person component and start working on the User input/output components. If you are reverting back to this commit, you can delete the user input/output folders entirely.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React app!</h1>
-        <p>This is really working!</p>
-        {/*<Person /> is calling the person component from Person.js file*/}
-        <Person name ="Colman" age="26"/>
-        <Person name="Max" age="28">My Hobbies: Racing</Person>
-        <Person name="Marco" age="27"/>
-      </div>
-    );
-    //The above code is not HTML but JSX, it's compiled to the exact same as the below code
-    //([element that's rendered to the DOM], [configuration (JS Object)], [children...])
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
-  }
+    state = {
+        persons: [
+            {name: 'Colman', age: 26},
+            {name: 'Marco', age: 27},
+            {name: 'Max', age: 28}
+        ],
+        otherState: 'some other value'
+    }
+
+    switchNameHandler = (newName) => {
+        // console.log('Was clicked!');
+        // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+        this.setState({
+            persons: [
+                {name: newName, age: 8},
+                {name: 'Manu', age: 9},
+                {name: 'Stephanie', age: 37}
+            ]
+        })
+    }
+
+    nameChangedHandler = (event) => {
+        this.setState({
+            persons: [
+                {name: 'Max', age: 38},
+                {name: event.target.value, age: 39},
+                {name: 'Stephanie', age: 36}
+            ]
+        })
+    }
+
+    render() {
+        const style = {
+          backgroundColor: 'white',
+          font: 'inherit',
+          border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
+
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <p>This is really working!</p>
+                <button
+                    style={style}
+                    onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}/>
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    click={this.switchNameHandler.bind(this, 'Max!')}
+                    changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}/>
+            </div>
+        );
+        // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+    }
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import './Person.css'
+
 const person = (props) => {
-    return <p>I'm {props.name} and I am {props.age} years old!</p>
+    return (
+        <div className="Person">
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            {/*CS children accesses anything b/w the opening and closing tags of a JSX element*/}
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
+        </div>
+    )
 };
 
 export default person;
